4ffe076a2d0a968d3a7f4ef3c7931a5b4de27935
ob198-CPU <obata1986@gmail.com>
2026-07-15 11:09:25 +0900

    専用スプレッドシート保存先へ接続

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 ﻿// Google Apps Script 縺ｮ繧ｦ繧ｧ繝悶い繝励ΜURL繧定ｨｭ螳壹☆繧九→縲∝・譛峨せ繝励Ξ繝・ラ繧ｷ繝ｼ繝井ｿ晏ｭ倥↓蛻・ｊ譖ｿ繧上ｊ縺ｾ縺吶・// 萓・ window.PRODUCTION_REPORT_API_URL = "https://script.google.com/macros/s/AKfycbxSd7Mwp_Nrmqfg_zAx-SFTkfYAijdVrnWFMrO2ebNryl35bil8SdVoOixUXII6uxEETw/exec";
-window.PRODUCTION_REPORT_API_URL = "https://script.google.com/macros/s/AKfycbxSd7Mwp_Nrmqfg_zAx-SFTkfYAijdVrnWFMrO2ebNryl35bil8SdVoOixUXII6uxEETw/exec";
+window.PRODUCTION_REPORT_API_URL = "https://script.google.com/macros/s/AKfycbwdsedtEdaD_5JQjSZoSZIkgN2jP3XjchuSm65D_2ZoV_OKxf-GWkN5-4s6jKeFyFS3pg/exec?systemKey=3";
 
 
 
